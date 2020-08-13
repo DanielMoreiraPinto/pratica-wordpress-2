bf45194298e3a6b5ee010872077e7e100293ee80
DanielMoreiraPinto <danielmoreirapinto.ti@gmail.com>
2020-08-13 12:31:42 -0300

    Javascript sendo incluído no functions.

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@
 
     add_action('wp_enqueue_scripts', 'temaPratica_resources');
 
-    
+    /* Add Js scripts */
+
+    wp_register_script('breakpoints', get_template_directory_uri() . '/assets/js/breakpoints.min.js');
+    wp_enqueue_script('breakpoints');
+
+    wp_register_script('browser', get_template_directory_uri() . '/assets/js/browser.min');
+    wp_enqueue_script('browser');
 
 ?>
